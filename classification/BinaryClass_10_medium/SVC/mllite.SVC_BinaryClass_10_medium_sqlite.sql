0bc10bf9620df055df91e26659d589810fe1ad4f
Antoine CARME <antoine.carme@outlook.com>
2024-04-08 21:57:17 +0200

    Added Model Explanation

--- classification/BinaryClass_10_medium/SVC/mllite.SVC_BinaryClass_10_medium_sqlite.sql
+++ classification/BinaryClass_10_medium/SVC/mllite.SVC_BinaryClass_10_medium_sqlite.sql
@@ -1,7 +1,7 @@
 WITH model_input AS 
  (SELECT "ADS"."index" AS "index",
     CAST("ADS"."X_0" AS FLOAT) AS "X_0", CAST("ADS"."X_1" AS FLOAT) AS "X_1", CAST("ADS"."X_2" AS FLOAT) AS "X_2", CAST("ADS"."X_3" AS FLOAT) AS "X_3", CAST("ADS"."X_4" AS FLOAT) AS "X_4", CAST("ADS"."X_5" AS FLOAT) AS "X_5", CAST("ADS"."X_6" AS FLOAT) AS "X_6", CAST("ADS"."X_7" AS FLOAT) AS "X_7", CAST("ADS"."X_8" AS FLOAT) AS "X_8", CAST("ADS"."X_9" AS FLOAT) AS "X_9"
-  FROM "MLLITE_INPUT_TABLE" AS "ADS" 
+  FROM "BinaryClass_10_medium" AS "ADS" 
  ),
 "SV_data" AS
  ( SELECT 
